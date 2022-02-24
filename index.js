bdbfecb1bdc1c497d10507d0f69fbcdc8c114e31
Tabatha Gamboa <tabathagamboa9@gmail.com>
2022-02-24 01:00:24 -0300

    hbs

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const {create} = require("express-handlebars");
+
+const app = express();
+// configurar partials handlebars
+const hbs = create({
+    partialsDir:["view/partials"],
+    extname:".hbs",
+})
+
+// motor de plantilla 
+app.engine(".hbs", hbs.engine)
+app.set('view engine' , '.hbs')
+app.set('views' , './view')
+
+// midleware
+app.use(express.static(__dirname + "/public"))
+app.use(
+    "/css/bootstrap.min.css",
+    express.static(
+        __dirname + "/node_modules/bootstrap/dist/css/bootstrap.min.css"
+    )
+);
+
+// app.use js
+
+app.get("/mercado", (req, res) => {
+    res.render("dashboard", {
+        frutas: ["cebollas", "lechuga","papas","pimenton", "tomate", "banana"],
+    });
+});
+
+
+
+
+app.listen(5000, () => console.log("servidor andando"))
